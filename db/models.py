c8a8b15c89d7e423218b2972d63b54a3faf60a09
roxxorarc <roxxorarc@gmail.com>
2026-04-03 22:03:57 +0200

    reorganization & fix compose

--- db/models.py
+++ db/models.py
@@ -3,3 +3,12 @@ from sqlalchemy import Column, Integer, String
 
 Base = declarative_base()
 
+class User(Base):
+    __tablename__ = "users"
+    id = Column(Integer, primary_key=True, index=True)
+    discord_id = Column(String, unique=True, index=True)
+
+class Character(Base):
+    __tablename__ = "characters"
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, index=True)
